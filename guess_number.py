5033fc6d878b962e51a71c75b2f84fb1053df17d
deviaze <dev@deviaze.com>
2024-02-06 20:01:09 -0600

    added hardcoded 3-guess limit

--- guess_number.py
+++ guess_number.py
@@ -4,13 +4,18 @@
 
 number = 10
 print("I'm thinking of a number...")
-    
+
+guesses_left = 3
 while guess := input("What number am I thinking of? "):
-    if guess.strip() == str(number):
-        print("Congratulations! You guessed the right number.")
-    elif guess == "q":
-        print(f"Game quit; number was {number}")
+    if guesses_left > 1:
+        if guess.strip() == str(number):
+            print("Congratulations! You guessed the right number.")
+        elif guess == "q":
+            print(f"Game quit; number was {number}")
+        else:
+            guesses_left -= 1
+            print(f"{guesses_left} guesses left! Please try again (or q to quit)")
+            continue
     else:
-        print(f"Sorry! Please try again (or q to quit)")
-        continue
+        print(f"You ran out of guesses! The number was {number}.")
     break
