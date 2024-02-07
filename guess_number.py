3dd7c9bb2fbbe6844308381fdb5ea7f3ae0c9ba4
deviaze <dev@deviaze.com>
2024-02-06 19:25:03 -0600

    Added while loop to repeat guess prompt until number guessed or 'q' pressed. Attempted to retain pre-existing code.

--- guess_number.py
+++ guess_number.py
@@ -4,9 +4,13 @@
 
 number = 10
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}")
+    
+while guess := input("What number am I thinking of? "):
+    if guess.strip() == str(number):
+        print("Congratulations! You guessed the right number.")
+    elif guess == "q":
+        print(f"Game quit; number was {number}")
+    else:
+        print(f"Sorry! Please try again (or q to quit)")
+        continue
+    break
